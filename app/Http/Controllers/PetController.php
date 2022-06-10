1afdac956e4c86dd2c767cb0771c2aad79136446
Rômulo Melendez Lima <romulomelendez.97@gmail.com>
2022-06-10 13:49:52 -0300

    controller name update

--- app/Http/Controllers/PetController.php
+++ app/Http/Controllers/PetController.php
@@ -2,11 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Pet;
 use Illuminate\Http\Request;
 
 class PetController extends Controller
 {
-    public function index(){
-        echo "<h1>Pet</h1>";
+    public function getPet() {
+        return Pet::get();
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -19,4 +19,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/pets', [PetController::class, 'index']);
+Route::get('/pets', [PetController::class, 'getPet']);
