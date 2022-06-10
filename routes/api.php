d240c8295690d6437271b02bbd889c1b3b5833fe
Rômulo Melendez Lima <romulomelendez.97@gmail.com>
2022-06-10 13:01:22 -0300

    creating a controller

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PetController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -18,6 +19,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/pets', function () {
-    echo "<h1>Pets</h1>";
-});
+Route::get('/pets', [PetController::class, 'index']);
